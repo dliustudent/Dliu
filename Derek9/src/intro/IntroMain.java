1401ae79234b11d7ce4bdcad4f8d1f3203e70006
BT_1N3_18 <BT_1N3_18@BT_1N3_18.bktech.local>
2017-09-11 15:29:56 -0400

    n

--- Derek9/src/intro/IntroMain.java
+++ Derek9/src/intro/IntroMain.java
@@ -3,5 +3,5 @@ package intro;
 public class IntroMain { 
 //line of text
   //another line of text
-  //whats up world im gonna become a great coder 
+  //whats up world im gonna become a great coder lets go 
 }
