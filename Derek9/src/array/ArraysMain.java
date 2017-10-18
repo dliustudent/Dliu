e2f386e087254a9e138edecc73dc376de613810d
BT_1N3_18 <BT_1N3_18@BT_1N3_18.bktech.local>
2017-10-18 14:14:39 -0400

    10/18

--- Derek9/src/array/ArraysMain.java
+++ Derek9/src/array/ArraysMain.java
@@ -1,130 +1,219 @@
 package array;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class ArraysMain {
-	
-	private String[] suits;
-	private String[] values;
-	private int[] testArray;
-	
-	public ArraysMain() {
-		suits = new String[4];
-		suits[0] = "Clubs";
-		suits[1] = "Diamonds";
-		suits[2] = "Heart";
-		suits[3] = "Spades";
-		values = new String[13];
-		for (int i =0; i<values.length;i++) {
-			values[i] = "" + (i+1);
-		}
-		
-		values[0] = "ace";
-		values[12] = "king";
-		values[11] = "queen";
-		values[10] = "jack";
-		printDeck();
-//		System.out.println(Arrays.toString(testArray));
-	}
-	
-	private void printDeck() {
-		for(int i =0; i < suits.length; i++) {
-			for(int k =0; k <values.length; k++) {
-				System.out.println(values[k] +" of " + suits[i]);
-			}
-		}
-		
-		/*
-		 *  for(String suit: suits){
-		 * 	for(String value: values){
-		 * 	deck[index] = value + "of" + suit;
-		 * index++
-		 * }
-		 * }
-		 * return deck;
-		 * }
-		 * 
-		 */
-	}
 
-	private void shuffle(int[] arr) {
-		for (int i =0;i <arr.length; i++) {
-			swap(arr,i,(int)(Math.random()*arr.length));
-		}
-	}
+   private int[] testArray;
+   private String[] suits;
+   private String[] values;
+  
+  
+  
+   public ArraysMain() {
+      
+       //suits = new String[4];
+       //suits[0] = "Diamonds";
+       //suits[1] = "Clubs";
+       //suits[2] = "Hearts";
+       //suits[3] = "Spades";
+       //values = new String[13];
+       //for(int i=0; i<values.length; i++)
+       //{
+       //  values[i] = ""+(i+1);
+          
+       //}
+       //values[0] = "Ace";
+       //values[12] ="King";
+       //values[11] ="Queen";
+       //values[10] ="Jack";
+       //printDeck();
+      
+       warmUpMethods();
+       tuesdayMethods();
+       //testArray = new int[5];
+       //populate(testArray);
+       //populate1ToN(testArray);
+      
+       //shuffle(testArray);
+       //countOccurences(testArray,2,12);
+      
+       //reverseOrder(testArray);
+      
+       //System.out.println(Arrays.toString(testArray));
+   }
+  
+   private void tuesdayMethods() {
+       // TODO Auto-generated method stub
+       int[] orderTest = {1,2,3,2,5,6,7,8,9,10};
+       longestConsecutiveSequence(orderTest);
+       System.out.println(longestConsecutiveSequence(orderTest));
+   }
+   /** remove the element at index 0, pushes all other elements forward
+    * 1 goes to 0,2 goes to 1,
+    * put the element that was at index 0 at the end
+    * @param arr
+    */
+   private void frontToBack(int[] arr) {
+       int [] saved = arr;
+       int x = arr[0];
+       for(int i = 0 ; i< arr.length-1; i++) {
+           arr[i] = saved[i+1];
+       }
+       arr[arr.length-1] = x;
+   }
+  
+   private void cycleThrough(int[] orderTest, int n) {
+       for(int i = 0; i<n;i++) {
+           frontToBack(orderTest);
+       }
+   }
+   private int longestConsecutiveSequence(int[] arr) {
+       int num = 1;
+       int highnum = 1;
+       for(int i = 0; i < arr.length-1;i++) {
+           if(isConsecutive(arr,i,i+1)) {
+               num ++;
+               if(num > highnum) {
+                   highnum = num;
+               }
+           }else {
+               num = 1;
+           }
+       }
+       return highnum;
+   }
+   private boolean isConsecutive(int[] arr,int start, int end) {
+       boolean check = false;
+       if(arr[start]+1 == arr[end]) {
+           check = true;
+       }
+       return check;
+   }
+   private void warmUpMethods() {
+       int[] orderTest = {1,2,3,4,5,6,7,8,9,10};
+       reverseOrder(orderTest);
+       System.out.println(Arrays.toString(orderTest));
+       System.out.println(Arrays.toString(subArray(orderTest,3,4)));
+   }
+  
+  
+   private void reverseOrder(int [] arr)
+   {
+        for(int i=0; i<arr.length/2; i++)
+        {
+            swap(arr,i,arr.length-1-i);
+        }
+  
+   }
+   private int[] subArray(int[] arr,int psn,int length) {
+       int[] sub = new int[length];
+       for(int i = 0; i< length; i++) {
+           sub[i] = arr[i+psn];
+       }
+       return sub;
+   }
+  
+  
+   private void printDeck()
+   {
+       for(String suit: suits)
+       {
+           for(String values: values)
+           {
+               System.out.println(values + " of "+suit);
+           }
+       }
+      
+   }
+  
+   private void countOccurences(int[] arr, int start, int end)
+   {
+       int[] counter = new int[end-start+1];
+       for(int value: arr)
+       {
+           counter[value-start]++;
+       }
+       for(int i=0; i<counter.length; i++)
+       {
+           System.out.println("The value "+(i+start)+" was rolled "+counter[i]+" times");
+       }
+   }
+  
+  
+   private void shuffle(int[] arr) {
+       for(int i= 0; i<arr.length; i++)
+       {
+           swap(arr,i,(int)(Math.random()*arr.length));
+       }
+  
+   }
+  
+   private void swap(int[] arr, int i, int j)
+   {
+       int placeholder = arr[i];
+       arr[i] = arr[j];
+       arr[j] = placeholder;
+   }
+  
+   private void populate1ToN(int[] arr)
+   {
+       for(int i=0; i<arr.length; i++)
+       {
+           arr[i]=(i+1);
+       }
+   }
+  
+  
+   private void populate(int[] arr)
+   {
+       for(int i=0; i<arr.length; i++)
+       {
+           arr[i] = diceRoll(2);
+       }
 
-	private void swap(int[] arr, int i, int j) {
-		int placeholder = arr[i];
-		arr[i] = arr[j];
-		arr[j] = placeholder;
-	}
+      
+   }
+  
+  
+   public void arrayNotes()
+   {
+       //two ways to make an array
+       int[] firstWay = {0,1,2,3,4,5};
+      
+       String[] secondWay = new String[5];
+       //secondWay[0] = 1;
+       //secondWay[1] = 10;
+      
+       //TWO WAYS TO ITERATE THROUGH AN ARRAY
+       for(int i=0; i<secondWay.length; i++)
+       {
+           System.out.println("The #"+i+" element is "+secondWay[i]);
+       }
+       //For each int in secondWay
+       for(String value: secondWay)
+       {
+           System.out.println("Element is "+value);
+       }
+       //primitive arrays are automatically populated with 0s
+       //object arrays are not populated
+   }
+  
+  
+   public static void main(String[] agrs)
+   {
+       ArraysMain sample = new ArraysMain();
+   }
 
-	private void populate1ToN(int[] arr) {
-		int count = 0;
-		for(int i =0; i <arr.length; i ++) {
-			arr[i]=(i+1);
-		}
-		
-	}
-
-	private void countOccurences(int[] arr, int start, int end) {
-		int[] counter = new int[end - start +1];
-		for (int value: arr) {
-			counter[value-start]++;
-		}
-		for(int i = 0 ; i < counter.length; i++) {
-			System.out.println("The value " + (i+start)+ " was rolled " +counter[i]+" times.");
-		}
-	}
-
-	private void populate(int[] arr) {
-		for(int i = 0; i < arr.length; i++) {
-			arr[i] = diceRoll(2);
-		}
-	}
-
-	public void arrayNotes() {
-		//two ways to construct an array:
-				int[] firstWay = {0,1,2,3,4,5};
-				//this will only work with the declaration.
-				//will not work:
-				//firstWay = {6,7,8,9,10};
-				
-				String[] secondWay = new String[5];
-				//you can go on like so, creating values at each index:
-				//secondWay[0] = 1;
-			//	secondWay[1] = 10;
-				
-				//TWO WAYS TO ITERATE THROUGH AN ARRAY:
-				for(int i = 0; i <secondWay.length; i ++ ) {
-					System.out.println("The # " + i + " element is " + secondWay[i]);
-				}
-				//"for each int in secondWay"
-				for(String value: secondWay) {
-					System.out.println("Element is "+value);
-				}
-				//NOTE: primitive arrays are auto-populated with 0's
-				//objects arrays are not populated(null)
-	}
-	
-	public static void main(String[] args) {
-		ArraysMain sample = new ArraysMain();
-		//1. Arrays are collections of primitives and Objects
-		//special note: this is the only collection of primitives
-		
-		//2. size cannot be edited
-		
-		//3. elements of an array are references to objects. In other 
-		// words, changing an element of an array changes the 
-		// references, not the object
-	}
-	
-	public int diceRoll(int n) {
-		int sum = 0;
-		for(int i =0; i < n ; i ++) {
-			int dieRoll = (int)(Math.random()*6)+1;
-			sum += dieRoll;
-		}
-		return sum;
-	}
+   public int diceRoll(int n)
+   {
+       int sum = 0;
+       for(int i = 0; i<n; i++)
+       {
+           int dieRoll = (int) (Math.random()*6)+1;
+           sum = sum + dieRoll;
+       }
+       return sum;
+   }
 }
