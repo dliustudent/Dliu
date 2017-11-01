08853bf3906e0a367bb9bb964630364077fc9150
BT_1N3_18 <BT_1N3_18@BT_1N3_18.bktech.local>
2017-11-01 15:32:16 -0400

    11/1

--- Derek9/src/caveExplorer/CaveRoom.java
+++ Derek9/src/caveExplorer/CaveRoom.java
@@ -138,15 +138,19 @@ public class CaveRoom {
 	 */
 	public static void setUpCaves() {
 		//1. Determine size of caves
-		CaveExplorer.caves = new CaveRoom[5][5];
+		CaveExplorer.caves = new NPCRoom[5][5];
 		CaveRoom[][] c = CaveExplorer.caves;//create a shortcut for accessing CaveExplorer.caves
 		//2. Populate with default caves
 		for(int row =0; row < c.length; row ++) {
 			for(int col = 0; col < c[row].length; col++) {
-				c[row][col] = new CaveRoom("This cave has coordinates "+row+", "+col);
+				c[row][col] = new NPCRoom("This cave has coordinates "+row+", "+col);
 			}
 		}
 		//3. Replace some default rooms with custom rooms (SAVE FOR LATER)
+		NPC testNPC = new NPC();
+		testNPC.setPosition(1, 2);
+		CaveExplorer.npcs = new NPC[1];
+		CaveExplorer.npcs[0] = testNPC;
 		
 		//4.set starting room
 		CaveExplorer.currentRoom = c[0][1];
@@ -156,8 +160,13 @@ public class CaveRoom {
 		c[0][1].setConnection(SOUTH, c[1][1], new Door());
 		c[1][1].setConnection(EAST, c[1][2], new Door());
 		
-		CaveRoom[][][] layers = new CaveRoom[3][][];
-		layers[0] = new CaveRoom[3][4];
+		/**
+		 * 
+		 *	THIS IS FOR 3D ARRAYS 
+		 * 
+		 * CaveRoom[][][] layers = new CaveRoom[3][][];
+		 *	layers[0] = new CaveRoom[3][4];
+		*/
 		//make doors lock after you walk in
 		//teleport to a different room
 		//make map dark
