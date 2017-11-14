8793c2ef59c04437eaf9136b801d36b45d224212
BT_1N3_18 <BT_1N3_18@BT_1N3_18.bktech.local>
2017-11-14 14:30:17 -0500

    11/14

--- Derek9/src/javathehardway/Habitat.java
+++ Derek9/src/javathehardway/Habitat.java
@@ -1,31 +1,30 @@
+
 package javathehardway;
 
-public class Habitat extends Environment{
-	
+public class Habitat extends Environment {
+
 	private Animal[] animals;
 	
-	public static void main(String args[]) {
+	public static void main(String[] args) {
 		Habitat h = new Habitat(5);
 		int count = 0;
-		while(count <6) {
+		while(count < 6 ) {
 			Animal a = new Animal();
 			h.addAnimal(a);
 			count++;
 		}
 		System.out.println(h);
 	}
-	
 	public Habitat(int livingCapacity) {
 		super(livingCapacity);
 		animals = new Animal[livingCapacity];
 	}
-	
 	public String toString() {
-		return super.toString()+"\nHabitants:\n" + animalReport();
+		return super.toString()+"\nHabitants:\n"+animalReport();
 	}
 	
 	public String animalReport() {
-		String s = "";
+		String s= "";
 		for(Animal a: animals) {
 			s += a+"\n";
 		}
@@ -38,22 +37,21 @@ public class Habitat extends Environment{
 	
 	public void addAnimal(Animal animal) {
 		int i =0;
-		while( i < animals.length) {
+		while(i< animals.length) {
 			if(animals[i] == null) {
 				animals[i] = animal;
 				break;
 			}
+			i++;
 		}
-		i++;
 	}
-
-	public void removeAnimal(int i) {
+	public void removeAnimal(int i ) {
 		animals[i] = null;
 	}
 	
 	public void removeAnimal(Animal a) {
-		for(int i =0; i <animals.length;i++) {
-			if(animals[i] == a)animals[i] = null;
+		for(int i = 0; i< animals.length; i++) {
+			if(animals[i] == a) animals[i] = null;
 		}
 	}
 }
